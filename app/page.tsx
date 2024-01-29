6bddb64ca9b91cb6851236a0a7d470b277fd3c84
Robinson Lam <rlam@playvox.com>
2024-01-29 18:35:09 +1100

    feature: add social icons

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@
 
 import { useState } from 'react';
 import ContactModal from '../components/ContactModal';
+import SocialFooter from '../components/SocialFooter';
 import Link from 'next/link'
 
 export default function Home() {
   const [contactFormActive, setContactFormActive] = useState(false);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center px-12">
+    <main className="flex min-h-screen flex-col items-center justify-center px-4 sm:px-12">
     
       { !contactFormActive && <div className="heroContainer container font-sans text-center flex flex-col gap-3 sm:gap-4">
         <p className="text-base sm:text-lg text-amber-400 leading-relaxed">Hi, I&apos;m Rob - welcome to my space.</p>
@@ -18,14 +19,18 @@ export default function Home() {
         
         <p className="text-xs text-gray leading-relaxed">Currently <strong>open</strong> to <strong>contract,</strong> <strong>freelance</strong> and <strong>full-time lead &amp; senior engineering</strong> opportunities</p>
       
-        <div className="callToActions flex flex-row my-2 justify-center">
-          <Link href="/contact">
-            <button className="border border-solid border-amber-500 bg-amber-500 hover:bg-amber-300 hover:border-amber-300 px-4 py-2 rounded-md text-white font-light mx-2 shadow-sm hover:shadow-md transition-all">Send me a message</button>
+        <div className="callToActions flex flex-col sm:flex-row my-2 justify-center">
+          <Link href="/contact" className="border border-solid border-amber-500 bg-amber-500 hover:bg-amber-300 hover:border-amber-300 px-4 py-2 rounded-md text-white font-light mb-4 sm:mb-0 sm:mx-2 shadow-sm hover:shadow-md transition-all">
+            Send me a message
           </Link>
-          <button className="border border-solid border-amber-500 bg-transparent hover:bg-amber-300 hover:border-amber-300 hover:text-white px-4 py-2 rounded-md text-amber-500 font-light mx-4 shadow-sm hover:shadow-md transition-all">My passion projects</button>
+
+          <button className="border border-solid border-amber-500 bg-transparent hover:bg-amber-300 hover:border-amber-300 hover:text-white px-4 py-2 rounded-md text-amber-500 font-light sm:mx-2 shadow-sm hover:shadow-md transition-all">My passion projects</button>
         </div>
 
       </div> }
+
+      {/* Social Footer */}
+      <SocialFooter />
 
       {/* Contact modal */}
       { contactFormActive && <ContactModal /> }
---
+++ components/SocialFooter.tsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+export default function SocialFooter() {
+    return (
+        <footer className="mt-12 border-t-2 border-amber-300 py-8 text-center w-1/2 sm:w-1/6">
+            <a href="https://github.com/robinsonlam">
+                <FontAwesomeIcon icon={faGithub} size="1x" className="mx-1 p-1 text-amber-400 hover:text-amber-300 transition"/>
+            </a>
+            <a href="https://www.linkedin.com/in/robinsonlam">
+                <FontAwesomeIcon icon={faLinkedin} size="1x" className="mx-1 p-1 text-amber-400 hover:text-amber-300 transition"/>
+            </a>
+        </footer>
+    )
+}
